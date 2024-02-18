349b76ca07398650ad92c41a5195c229887c1669
PREM DHADKAR <premdhadkar@users.noreply.github.com>
2024-02-18 16:11:27 +0530

    Transform a kafka log

--- src/main/java/com/example/loggingconsumer/LoggingConsumerApplication.java
+++ src/main/java/com/example/loggingconsumer/LoggingConsumerApplication.java
@@ -1,5 +1,5 @@
 package com.example.loggingconsumer;
-import java.util.function.Consumer;
+import java.util.function.Function;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,33 +13,7 @@ public class LoggingConsumerApplication {
 	}
 
 	@Bean
-	Consumer<Person> log1() {
-		return person -> {
-			System.out.println("Received: " + person);
-		};
-	}
-
-	@Bean
-	Consumer<Person> log2() {
-		return person -> {
-			System.out.println("Received: " + person);
-		};
-	}
-
-	
-	public static class Person {
-		private String name;
-
-		public String getName() {
-			return name;
-		}
-
-		public void setName(String name) {
-			this.name = name;
-		}
-
-		public String toString() {
-			return this.name;
-		}
+	protected Function<String, String> uppercase() {
+	    return value -> value.toUpperCase();
 	}
 }
--- src/test/java/com/example/loggingconsumer/LoggingConsumerApplicationTests.java
+++ src/test/java/com/example/loggingconsumer/LoggingConsumerApplicationTests.java
@@ -1,46 +1,28 @@
 package com.example.loggingconsumer;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.stream.binder.test.InputDestination;
+import org.springframework.cloud.stream.binder.test.OutputDestination;
 import org.springframework.cloud.stream.binder.test.TestChannelBinderConfiguration;
 import org.springframework.context.annotation.Import;
 import org.springframework.messaging.support.GenericMessage;
 
-@SpringBootTest(classes = LoggingConsumerApplication.class)
-@Import({ TestChannelBinderConfiguration.class })
-public class LoggingConsumerApplicationTests {
+@SpringBootTest(classes =  LoggingConsumerApplication.class)
+@Import({TestChannelBinderConfiguration.class})
+class BootTestStreamApplicationTests {
 
 	@Autowired
 	private InputDestination input;
 
+	@Autowired
+	private OutputDestination output;
+
 	@Test
-	void consumerTest() {
-		input.send(new GenericMessage<byte[]>("{\"name\":\"Sam Spade\"}".getBytes()));
-
-//		Redirect System.out to ByteArrayOutputStream
-		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-		PrintStream printStream = new PrintStream(outputStream);
-		PrintStream originalOut = System.out;
-		System.setOut(printStream);
-
-		input.send(new GenericMessage<byte[]>("{\"name\":\"Sam Spade\"}".getBytes()));
-
-		String capturedOutput = outputStream.toString();
-
-		System.setOut(originalOut);
-
-		assertTrue(capturedOutput.length() > 19);
-
-		String next19Characters = capturedOutput.substring(0, 19);
-		assertEquals("Received: Sam Spade", next19Characters);
-
+	void contextLoads() {
+		input.send(new GenericMessage<byte[]>("hello".getBytes()));
+		assertThat(output.receive().getPayload()).isEqualTo("HELLO".getBytes());
 	}
 }
